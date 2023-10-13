1629420bcc98f3aea8c1985613181a646c11c188
mralex07 <mister.alex07@google.com>
2023-10-13 23:01:59 +0300

    a_dux vial keymap

--- keyboards/a_dux/rules.mk
+++ keyboards/a_dux/rules.mk
@@ -1,1 +1,21 @@
 # This file intentionally left blank
+# Build Options
+#   change yes to no to disable
+#
+# RP2040-specific options
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+
+
+
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+
+SERIAL_DRIVER = vendor
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
+KEYBOARD_SHARED_EP = yes
+
+# OPT_DEFS += -DHAL_USE_I2C=TRUE
