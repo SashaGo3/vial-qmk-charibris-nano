5f0d2c791955ed44c1355126296f5173f42c0cde
mralex07 <50528263+mralex07@users.noreply.github.com>
2024-01-11 20:29:43 +0000

    spi

--- keyboards/a_dux_spi/mcuconf.h
+++ keyboards/a_dux_spi/mcuconf.h
@@ -22,11 +22,11 @@
 /* #undef RP_SPI_USE_SPI0
 #define RP_SPI_USE_SPI0 TRUE */
 
-#undef RP_I2C_USE_I2C0
-#define RP_I2C_USE_I2C0 FALSE
+//#undef RP_I2C_USE_I2C0
+//#define RP_I2C_USE_I2C0 FALSE
 
-#undef RP_I2C_USE_I2C1
-#define RP_I2C_USE_I2C1 TRUE
+//#undef RP_I2C_USE_I2C1
+//#define RP_I2C_USE_I2C1 TRUE
 
 #undef RP_SPI_USE_SPI1
 #define RP_SPI_USE_SPI1 TRUE
