e1a8e4b5b47b72510cb06c6dd268ac58e38f6ac7
mralex07 <50528263+mralex07@users.noreply.github.com>
2024-01-11 22:15:11 +0000

    I2C_DRIVER_REQUIRED = yes

--- keyboards/a_dux/rules.mk
+++ keyboards/a_dux/rules.mk
@@ -17,5 +17,5 @@ POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
 KEYBOARD_SHARED_EP = yes
-
+I2C_DRIVER_REQUIRED = yes
 # OPT_DEFS += -DHAL_USE_I2C=TRUE
